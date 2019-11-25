528ee0abc2dae322c9372c709b2023e4471ce9fc
javajason <jasonmailhub-git@yahoo.com>
2019-11-24 17:03:48 -0800

    update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t travisimages/multi-client:latest -t travisimages/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t travisimages/multi-server:latest -f travisimages/multiserver:$SHA -f ./server/Dockerfile ./server
+docker build -t travisimages/multi-server:latest -f travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t travisimages/multi-worker:latest -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push travisimages/multi-client:latest
@@ -11,6 +11,6 @@ docker push travisimages/multi-server:$SHA
 docker push travisimages/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=travisimages/multi-server:$SHA
 kubectl set image deployments/client-deployment client=travisimages/multi-client:$SHA
+kubectl set image deployments/server-deployment server=travisimages/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=travisimages/multi-worker:$SHA
