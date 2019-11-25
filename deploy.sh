a61c616efada33a3dfc9ac269f2a714d7350eef6
javajason <jasonmailhub-git@yahoo.com>
2019-11-25 14:27:39 -0800

    moved build copy into current dir 6

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,21 @@
 # docker build -t travisimages/multi-worker:latest -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 # docker build -t travisimages/multi-server:latest -t travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
 
-docker build -t travisimages/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker build -t travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
+## docker build -t travisimages/multi-client:$SHA -f ./client/Dockerfile ./client
+## docker build -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+## docker build -t travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
 
 docker build -t travisimages/multi-client:latest -f ./client/Dockerfile ./client
-docker build -t travisimages/multi-worker:latest -f ./worker/Dockerfile ./worker
-docker build -t travisimages/multi-server:latest -f ./server/Dockerfile ./server
+## docker build -t travisimages/multi-worker:latest -f ./worker/Dockerfile ./worker
+## docker build -t travisimages/multi-server:latest -f ./server/Dockerfile ./server
 
-docker push travisimages/multi-worker:$SHA
+## docker push travisimages/multi-worker:$SHA
 docker push travisimages/multi-client:$SHA
-docker push travisimages/multi-server:$SHA
+## docker push travisimages/multi-server:$SHA
 
-docker push travisimages/multi-client:latest
-docker push travisimages/multi-server:latest
-docker push travisimages/multi-worker:latest
+## docker push travisimages/multi-client:latest
+## docker push travisimages/multi-server:latest
+## docker push travisimages/multi-worker:latest
 
 kubectl apply -f k8s
 kubectl set image deployments/worker-deployment worker=travisimages/multi-worker:$SHA
