11bf6d7cbffee6a04ad6a623fd88e1810edd52eb
javajason <jasonmailhub-git@yahoo.com>
2019-11-25 08:22:55 -0800

    separate out tag builds

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
-docker build -t travisimages/multi-client:latest -t travisimages/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t travisimages/multi-worker:latest -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker build -t travisimages/multi-server:latest -f travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
+# docker build -t travisimages/multi-client:latest -t travisimages/multi-client:$SHA -f ./client/Dockerfile ./client
+# docker build -t travisimages/multi-worker:latest -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# docker build -t travisimages/multi-server:latest -t travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
+
+docker build -t travisimages/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t travisimages/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t travisimages/multi-server:$SHA -f ./server/Dockerfile ./server
+
+docker build -t travisimages/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t travisimages/multi-worker:latest -f ./worker/Dockerfile ./worker
+docker build -t travisimages/multi-server:latest -f ./server/Dockerfile ./server
 
 docker push travisimages/multi-client:latest
 docker push travisimages/multi-server:latest
@@ -12,9 +20,9 @@ docker push travisimages/multi-server:$SHA
 
 kubectl apply -f k8s
 # kubectl set image deployments/worker-deployment worker=travisimages/multi-worker:$SHA
-# kubectl set image deployments/client-deployment client=travisimages/multi-client:$SHA
+kubectl set image deployments/client-deployment client=travisimages/multi-client:$SHA
 # kubectl set image deployments/server-deployment server=travisimages/multi-server:$SHA
 
 kubectl set image deployments/worker-deployment worker=travisimages/multi-worker:latest
-kubectl set image deployments/client-deployment client=travisimages/multi-client:latest
+# kubectl set image deployments/client-deployment client=travisimages/multi-client:latest
 kubectl set image deployments/server-deployment server=travisimages/multi-server:latest
